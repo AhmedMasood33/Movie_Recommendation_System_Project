546cfac699729394d7eb68d6f16f326e2414d7bc
Ahmed-Moatazz <ahmedmoatz376@gmail.com>
2026-03-08 23:04:43 +0200

    Update Movie.java

--- model/Movie.java
+++ model/Movie.java
@@ -4,4 +4,25 @@ import java.util.List;
 
 public class Movie {
     
+    private final String title;
+    private final String movieId;
+    private final List<String> categories;
+
+    public Movie(String title, String movieId, List<String> categories) {
+        this.title = title;
+        this.movieId = movieId;
+        this.categories = categories;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public String getMovieId() {
+        return movieId;
+    }
+
+    public List<String> getCategories() {
+        return categories;
+    }
 }
